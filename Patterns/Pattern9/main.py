4201c6a573c4ea07b41f00c98158af7bf92bdd43
Aayushi Vinod <aayushi.vinod2507@gmail.com>
2026-08-03 09:50:49 +0530

    Practice

--- Patterns/Pattern9/main.py
+++ Patterns/Pattern9/main.py
@@ -5,12 +5,16 @@ class Solution:
                 print(" ", end="")
             for j in range(2*i+1):
                 print("*", end="")
+            for j in range(n-i-1):
+                print(" ", end="")
             print()
-        for i in range(n):
+        for i in range(n+1):
             for j in range(i):
                 print(" ", end="")
             for j in range(2*n-(2*i+1)):
                 print("*", end="")
+            for j in range(i):
+                print(" ", end="")
             print()
 if __name__=="__main__":
     sol=Solution()
